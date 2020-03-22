1ca2520644a8734d4fbd944d5d47b3ef630b1471
Adam <adam.wiktor.mlynarczyk@gmail.com>
2020-03-22 09:18:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const zadanieInput = document.getElementById('zadanie-input');
+const zadanieButton = document.getElementById('zadanie-button');
+const zadanieUsunButton = document.getElementById('usun-button');
+const sekcja = document.getElementById('div1');
+
+let close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+let nowyElement;
+
+function dodajZadanie() {
+  if (zadanieInput.value != '') {
+    nowyElement = document.createElement("h2");
+    nowyElement.innerHTML = '&#128313 ' + zadanieInput.value;
+    sekcja.appendChild(nowyElement);
+    zadanieInput.value = '';
+  } else {
+    alert('Napisz jakieś zadanie')
+  }
+
+  // krzyżyk na końcu zadania
+  let span = document.createElement("span");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  nowyElement.appendChild(span);
+
+  // usuwanie zadania po kliknięciu w krzyżyk
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      let div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+function usunZadanie() {
+  (sekcja.firstChild != null) ? sekcja.firstChild.remove():
+    alert('Brak zadań do usunięcia');
+}
+
+zadanieButton.addEventListener("click", dodajZadanie, false);
+zadanieUsunButton.addEventListener("click", usunZadanie, false);
