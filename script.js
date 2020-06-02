edb520c2e38c8081a31ed8f8bd0b42991e0e8a21
Adam <adam.wiktor.mlynarczyk@gmail.com>
2020-06-02 09:27:59 +0200

    text decoration

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-const zadanieInput = document.getElementById('zadanie-input');
-const zadanieButton = document.getElementById('zadanie-button');
-const zadanieUsunButton = document.getElementById('usun-button');
-const sekcja = document.getElementById('div1');
+const zadanieInput = document.getElementById("zadanie-input");
+const zadanieButton = document.getElementById("zadanie-button");
+const zadanieUsunButton = document.getElementById("usun-button");
+const sekcja = document.getElementById("div1");
 
 let close = document.getElementsByClassName("close");
 for (let i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     let div = this.parentElement;
     div.style.display = "none";
-  }
+  };
 }
 
 let nowyElement;
 
+const toggleClass = e => {
+  e.target.classList.toggle("zrobione");
+  console.log(e.target);
+};
+
 function dodajZadanie() {
-  if (zadanieInput.value != '') {
+  if (zadanieInput.value != "") {
     nowyElement = document.createElement("h2");
-    nowyElement.innerHTML = '&#128313 ' + zadanieInput.value;
+    nowyElement.innerHTML = "&#128313 " + zadanieInput.value;
     sekcja.appendChild(nowyElement);
-    zadanieInput.value = '';
+    zadanieInput.value = "";
+    nowyElement.classList.add("zadanie");
+    nowyElement.addEventListener("click", toggleClass);
   } else {
-    alert('Napisz jakieś zadanie')
+    alert("Napisz jakieś zadanie");
   }
 
   // krzyżyk na końcu zadania
@@ -35,13 +42,14 @@ function dodajZadanie() {
     close[i].onclick = function() {
       let div = this.parentElement;
       div.style.display = "none";
-    }
+    };
   }
 }
 
 function usunZadanie() {
-  (sekcja.firstChild != null) ? sekcja.firstChild.remove():
-    alert('Brak zadań do usunięcia');
+  sekcja.firstChild != null
+    ? sekcja.firstChild.remove()
+    : alert("Brak zadań do usunięcia");
 }
 
 zadanieButton.addEventListener("click", dodajZadanie, false);
